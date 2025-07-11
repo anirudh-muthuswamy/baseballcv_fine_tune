052a557e0ec1f2cc62cb6960e25d89a01e3b2691
Anirudh Muthuswamy <126427302+anirudh-muthuswamy@users.noreply.github.com>
2025-07-11 13:12:37 -0400

    cleaned up setup_data.py and included absolute path for yaml data filepaths

--- setup_data.py
+++ setup_data.py
@@ -17,33 +17,28 @@ import yaml
 import os
 from IPython.display import display
 
-dataset_path = "baseball_rubber_home_glove"
+def create_data_yaml(dataset_path):
+    dataset_path = os.path.abspath(dataset_path)
+    data = {'train' :  f'{dataset_path}/train/images',
+            'val' :  f'{dataset_path}/valid/images',
+            'test' :  f'{dataset_path}/test/images',
+            'nc': 4,
+            'names': ['glove','homeplate','baseball','rubber'],
+            }
 
-data = {'train' :  f'{dataset_path}/train/images',
-        'val' :  f'{dataset_path}/valid/images',
-        'test' :  f'{dataset_path}/test/images',
-        'nc': 4,
-        'names': ['glove','homeplate','baseball','rubber'],
-        }
+    # overwrite the data to the .yaml file
+    with open(f'baseball_data.yaml', 'w') as f:
+        yaml.dump(data, f)
 
-# overwrite the data to the .yaml file
-with open(f'baseball_data.yaml', 'w') as f:
-    yaml.dump(data, f)
-
-# read the content in .yaml file
-with open(f'baseball_data.yaml', 'r') as f:
-    hamster_yaml = yaml.safe_load(f)
-    display(hamster_yaml)
+    # read the content in .yaml file
+    with open(f'baseball_data.yaml', 'r') as f:
+        hamster_yaml = yaml.safe_load(f)
+        print("Data YAML content:")
+        display(hamster_yaml)
 
 
 # There is majorly only 4 classes (class ids: 0, 1, 2, 3) in this dataset (glove, homeplate, baseball, rubber)
 # remove class id: 4 that is not used in the dataset
-
-label_dirs = [
-    f'{dataset_path}/train/labels',
-    f'{dataset_path}/valid/labels',
-    f'{dataset_path}/test/labels'
-]
 
 # Function to remove lines with class 4
 def filter_labels(label_dir):
@@ -59,6 +54,19 @@ def filter_labels(label_dir):
         with open(label_path, 'w') as file:
             file.writelines(filtered_lines)
 
-# Process all label directories
-for label_dir in label_dirs:
-    filter_labels(label_dir)
+if __name__ == "__main__":
+    dataset_path = "baseball_rubber_home_glove"
+
+    create_data_yaml(dataset_path)
+    
+    label_dirs = [
+    f'{dataset_path}/train/labels',
+    f'{dataset_path}/valid/labels',
+    f'{dataset_path}/test/labels'
+    ]
+
+    # Process all label directories
+    for label_dir in label_dirs:
+        filter_labels(label_dir)
+
+    print("Filtered labels to remove class 4 annotations.")
